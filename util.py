46ef24861bd3b0c53a956bd710a9aba849e9c853
Artem Bolshakov <a.t.bolsh@gmail.com>
2020-11-21 07:42:04 -0500

    Parsing function added; minor code refactoring for consistency.

--- util.py
+++ util.py
@@ -2,8 +2,8 @@ import os
 import torch
 import math
 
-def writeManifest(path, totalRecords, batchSize, numBatches):
-    f = open(path, 'w')
+def writeManifest(dirname, totalRecords, batchSize, numBatches):
+    f = open(os.path.join(dirname, 'manifest'), 'w')
     s = "This directory is formatted for a BatchDataset.\n"
     s += "This file contains metadata.\n\n"
     s += "The total number of records is " + str(totalRecords) + "\n"
@@ -13,6 +13,18 @@ def writeManifest(path, totalRecords, batchSize, numBatches):
     f.close()
     return True
 
+
+def parseManifest(dirname):
+    f = open(os.path.join(dirname, 'manifest'), 'r') # Put this in a try-except
+    s = f.read()
+    f.close()
+    l = s.split('\n')[3:]
+    totalRecords = int(l[0].split(' ')[-1])
+    batchSize = int(l[1].split(' ')[-1])
+    numBatches = int(l[2].split(' ')[-1])
+    return totalRecords, batchSize, numBatches
+
+
 def verifyDirEmpty(dirname, cleanDir=False):
     if len(os.listdir(dirname)) > 0:
         if cleanDir:
@@ -21,6 +33,7 @@ def verifyDirEmpty(dirname, cleanDir=False):
         else:
             raise OSError("Directory '" + dirname  + "' not empty; choose a new directory or set cleanDir=True.")
     return True 
+
 
 def createFormattedDir(source, dirname, batchSize = 128, cleanDir=False, verbose=True):
     """Given a source that supports __len__ and __getitem__, 
@@ -33,7 +46,7 @@ to avoid waiting on a disk read."""
         print("Directory '" + dirname + "' is empty.")
     totalRecords = len(source)
     numBatches = math.ceil(totalRecords / batchSize)
-    if writeManifest(os.path.join(dirname, 'manifest'), totalRecords, batchSize, numBatches) and verbose:
+    if writeManifest(dirname, totalRecords, batchSize, numBatches) and verbose:
         print("Manifest written.")
 
     def _writeBatch(container, recordId):
@@ -42,7 +55,9 @@ to avoid waiting on a disk read."""
         torch.save(container, pathName)
         if verbose:
             print("Records up to " + str(recordId + 1) +  " saved; " + str(batchId + 1) + " batches written.")
-     
+
+    # Main loop to store all the records.
+    # By default, python list are the batches, but the BatchDataset class will work with any container.
     res = []
     for i in range(totalRecords):
         res.append(source[i])
